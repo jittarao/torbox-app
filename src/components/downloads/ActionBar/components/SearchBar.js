337e283aa7497d995daaa55f7c58b586f15ac818
Raghavender Rao Jitta <rag.jitta.klu@gmail.com>
2025-12-28 18:26:19 +0530

    Updated search placeholder text

--- src/components/downloads/ActionBar/components/SearchBar.js
+++ src/components/downloads/ActionBar/components/SearchBar.js
@@ -1,7 +1,7 @@
 import Icons from '@/components/icons';
 import { useTranslations } from 'next-intl';
 
-export default function SearchBar({ search, onSearchChange }) {
+export default function SearchBar({ search, onSearchChange, itemTypePlural }) {
   const t = useTranslations();
   return (
     <div className="relative flex-1">
@@ -13,12 +13,12 @@ export default function SearchBar({ search, onSearchChange }) {
       </div>
       <input
         type="text"
-        placeholder={t('SearchBar.placeholder', {
-          itemType: t('Common.itemTypes.torrents'),
+        placeholder={t('SearchBar.placeholderDownloads', {
+          itemType: itemTypePlural,
         })}
         value={search}
         onChange={(e) => onSearchChange(e.target.value)}
-        className="w-full pl-10 pr-10 py-2 min-w-64 md:min-w-[26rem] rounded-lg border border-border dark:border-border-dark 
+        className="w-full pl-10 pr-10 py-2 min-w-64 rounded-lg border border-border dark:border-border-dark 
           bg-transparent text-primary-text dark:text-primary-text-dark 
           placeholder-primary-text/50 dark:placeholder-primary-text-dark/50
           focus:outline-none focus:ring-2 focus:ring-accent/20 dark:focus:ring-accent-dark/20 
